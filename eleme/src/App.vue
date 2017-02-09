879c442e82fc5ebcda39055bb9f6a3b394ce71e8
masterX <x13133053566@163.com>
2017-02-09 23:19:48 +0800

    v1.01

--- eleme/src/App.vue
+++ eleme/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <tmpheader></tmpheader>
-    <nav class="tab">
+    <nav class="tab border-1px">
       <div class="tab-item">
          <router-link to="/goods" title="商品">商品</router-link>
       </div>
@@ -27,13 +27,23 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
   .app
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      //border-bottom: 1px solid rgba(7,17,27,0.1)
+      border-1px(rgba(7,17,27,0.1))
       .tab-item
         flex: 1
         text-align: center
+        a
+          display: block
+          font-style: 14px
+          color: rgb(77,85,93)
+          &.active
+            color: rgb(240,20,20)
 </style>
--- eleme/src/main.js
+++ eleme/src/main.js
@@ -2,12 +2,44 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+// import router from './router';
+import VueRouter from 'vue-router';
 
-/* eslint-disable no-new */
+import Goods from 'components/goods/goods';
+import Ratings from 'components/ratings/ratings';
+import Seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';//引入主css文件 路径名需要在base中定义
+
+const routes = [//设置路由的页面
+  {
+    path: '/goods',
+    name: 'goods',
+    component: Goods,
+  },
+  {
+    path: '/ratings',
+    name: 'ratings',
+    component: Ratings,
+  },
+  {
+    path: '/seller',
+    name: 'seller',
+    component: Seller,
+  }
+];
+
+var router = new VueRouter({
+  routes: routes,
+  linkActiveClass: 'active'//改变当前选中的路由
+});
+
+Vue.use(VueRouter);
+router.push('/goods');//视频所讲router.go  改为push方法改变默认渲染的
+
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
   components: { App },
+  router
 });
