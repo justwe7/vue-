cbc07e4507bc37f2b82cb8adcf3f73a580efe44d
masterX <x13133053566@163.com>
2017-03-08 22:21:55 +0800

    商品详情页添加自定义滚动

--- eleme/src/components/goods/goods.vue
+++ eleme/src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -9,25 +9,25 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list" ref="foodList">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item">
               <div class="icon">
-                <img :src="food.icon">
+                <img width="57" height="57" :src="food.icon">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
+                  <span class="count">月售{{food.sellCount}}份</span>
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,6 +39,9 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BSscroll from "better-scroll";
+
+
   const ERR_OK = 0;
 
   export default {
@@ -49,7 +52,8 @@
       },
       data() {
         return {
-            goods: []
+            goods: [],//接收传过来的数据
+            listHeight: []
         }
       },
       created() {
@@ -59,9 +63,52 @@
               response = response.body;
               if(response.errno === ERR_OK){
                 this.goods = response.data;
-                console.log(this.goods)
+                this.$nextTick(() => {
+                  this._initScroll();
+                  this._calculateHeight();
+                });
+//                console.log(this.goods)
               }
           })
+      },
+      computed: {//会实时变化
+          currentIndex() {
+            for (let i = 0; i < this.listHeight.length; i++) {
+              let height1 = this.listHeight[i];
+              let height2 = this.listHeight[i + 1];
+              if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                return i;
+              }
+            }
+            return 0;
+          }
+      },
+      methods: {
+        _initScroll() {//自定义的滚动条
+          this.meunScroll = new BSscroll(this.$refs.menuWrapper, {
+            click: true
+          });
+
+          this.foodsScroll = new BSscroll(this.$refs.foodsWrapper, {
+            click: true,
+            probeType: 3
+          });
+
+          this.foodsScroll.on('scroll', (pos) => {
+            this.scrollY = Math.abs(Math.round(pos.y));
+          });
+        },
+        _calculateHeight() {
+          let foodList = this.$refs.foodList;
+          let height = 0;
+          this.listHeight.push(height);
+          for (let i = 0; i < foodList.length; i++) {
+            let item = foodList[i];
+            height += item.clientHeight;
+            console.log(height)
+            this.listHeight.push(height);
+          }
+        }
       }
   };
 </script>
@@ -111,4 +158,56 @@
         font-size: 12px
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 12px
 </style>
